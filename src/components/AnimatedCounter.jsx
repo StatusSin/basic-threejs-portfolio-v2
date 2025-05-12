40f65ef4089870f0f0d1f92e91483b68d2678b7b
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-12 12:55:01 -0500

    Hero Complete
    the hero section is implemented fully

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,12 +1,16 @@
 import {counterItems} from "../constants/index.js";
+import CountUp from "react-countup";
 
 const AnimatedCounter = () => {
     return (
         <div id = "counter" className = "passing-x-lg xl:mt-0 mt-32">
             <div className = "mx-auto grid-4-cols">
                 {counterItems.map((item) => (
-                    <div key = {counterItems.label} className = "counter-number text-white text-5xl font-bold mb-2">
-                        0 {item.suffix}
+                    <div className = "bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+                        <div key = {counterItems.label} className = "counter-number text-white text-5xl font-bold mb-2">
+                            <CountUp suffix = {item.suffix} end={item.value} duration = {3}/>
+                        </div>
+                        <div className = "text-white-50 text-lg">{item.label}</div>
                     </div>
                 ))}
             </div>

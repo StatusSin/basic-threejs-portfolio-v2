be9f1c03f797fb7dc125a6a7ab1001a7367b3fce
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-11 23:56:51 -0500

    Values Exist
    Numbers positions in place for the counter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -4,7 +4,11 @@ const AnimatedCounter = () => {
     return (
         <div id = "counter" className = "passing-x-lg xl:mt-0 mt-32">
             <div className = "mx-auto grid-4-cols">
-                {counterItems}
+                {counterItems.map((item) => (
+                    <div key = {counterItems.label} className = "counter-number text-white text-5xl font-bold mb-2">
+                        0 {item.suffix}
+                    </div>
+                ))}
             </div>
         </div>
     )

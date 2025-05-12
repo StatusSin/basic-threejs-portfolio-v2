0b27af6d8951ea1ad0f6277258b621f2974f2935
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-11 19:13:03 -0500

    AnimatedCounter basic
    basic implementation of animated counter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import {counterItems} from "../constants/index.js";
 
 const AnimatedCounter = () => {
     return (
-        <div>AnimatedCounter</div>
+        <div id = "counter" className = "passing-x-lg xl:mt-0 mt-32">
+            <div className = "mx-auto grid-4-cols">
+                {counterItems}
+            </div>
+        </div>
     )
 }
 export default AnimatedCounter

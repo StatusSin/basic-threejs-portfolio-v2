083c72feebc2c5ee876f9990eabf7d1fc7e7cd48
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-14 19:28:27 -0700

    Navbar more
    more robust navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {navLinks} from "../constants/index.js";
 
 const NavBar = () => {
     return (
@@ -9,7 +9,16 @@ const NavBar = () => {
                 </a>
 
                 <nav className = "desktop">
-
+                    <ul>
+                        {navLinks.map(({ link, name }) => (
+                            <li key = {name} className = "group">
+                                <a href = {link}>
+                                    <span>{name}</span>
+                                    <span></span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </nav>
 
                 <a href = "#contact" className = "contact-btn group">

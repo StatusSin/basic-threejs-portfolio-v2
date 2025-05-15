227ef1b519d7fc224328534db190cf38ac1b69cf
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-14 19:11:24 -0700

    Navbar Basic
    the navbar exists

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header className = "navbar">
+            <div className = "inner">
+                <a className = "logo" href = "#hero">
+                    Aryaman Ramchandran
+                </a>
+
+                <nav className = "desktop">
+
+                </nav>
+
+                <a href = "#contact" className = "contact-btn group">
+                    <div className = "inner">
+                        <span>Contact Me</span>
+                    </div>
+                </a>
+            </div>
+        </header>
+    )
+}
+export default NavBar

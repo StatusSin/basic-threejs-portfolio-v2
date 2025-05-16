8143c3209d9618954c2aec8c77434d6b4ec931b6
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-15 23:26:45 -0700

    NavBar implemented
    navbar completely implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,48 @@
 import {navLinks} from "../constants/index.js";
+import {useEffect, useState} from "react";
 
 const NavBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10;
+            setScrolled(isScrolled);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <header className = "navbar">
-            <div className = "inner">
-                <a className = "logo" href = "#hero">
+        <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+            <div className="inner">
+                <a href="#hero" className="logo">
                     Aryaman Ramchandran
                 </a>
 
-                <nav className = "desktop">
+                <nav className="desktop">
                     <ul>
                         {navLinks.map(({ link, name }) => (
-                            <li key = {name} className = "group">
-                                <a href = {link}>
+                            <li key={name} className="group">
+                                <a href={link}>
                                     <span>{name}</span>
-                                    <span className = "underline" />
+                                    <span className="underline" />
                                 </a>
                             </li>
                         ))}
                     </ul>
                 </nav>
 
-                <a href = "#contact" className = "contact-btn group">
-                    <div className = "inner">
-                        <span>Contact Me</span>
+                <a href="#contact" className="contact-btn group">
+                    <div className="inner">
+                        <span>Contact me</span>
                     </div>
                 </a>
             </div>
         </header>
-    )
+    );
 }
-export default NavBar
+
+export default NavBar;

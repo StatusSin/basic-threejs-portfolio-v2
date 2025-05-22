9bc798c51adad888da2fde6da77fad152d225fd8
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-21 18:28:26 -0700

    Experience Section
    Begin experience section

---
+++ src/components/TitleHeader.jsx
@@ -0,0 +1,10 @@
+const TitleHeader = ({ title, sub }) => {
+    return (
+        <div className = "flex flex-col items-center gap-5">
+            <div className = "hero-badge">
+                <p>{sub}</p>
+            </div>
+        </div>
+    )
+}
+export default TitleHeader

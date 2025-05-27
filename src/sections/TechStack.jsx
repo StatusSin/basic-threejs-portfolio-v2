e8db9926750ca365b5d0abc66c7357a5b742bdaf
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-27 18:20:55 -0500

    TechStack section

---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TitleHeader from "../components/TitleHeader.jsx";
+
+const TechStack = () => {
+    return (
+        <div id = "skills" className = "flex-center section-padding">
+            <div>
+                <TitleHeader />
+            </div>
+        </div>
+    )
+}
+export default TechStack

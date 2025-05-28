a1a644c9b34560b8a58642a49b92c434b711e61a
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-05-28 16:24:11 -0500

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -4,11 +4,15 @@ import TitleHeader from "../components/TitleHeader.jsx";
 const TechStack = () => {
     return (
         <div id = "skills" className = "flex-center section-padding">
-            <div>
+            <div className = "w-full h-full md:px-10 px-5">
                 <TitleHeader
                     title = "Tech Stacks"
                     sub = "What I use"
                 />
+
+                <div className = "tech-grid">
+
+                </div>
             </div>
         </div>
     )

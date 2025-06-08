3d457b90c66628c1615959f71a076b9590d2f77b
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-08 18:48:17 -0500

    Useless Section

--- src/pages/PortfolioHome.jsx
+++ src/pages/PortfolioHome.jsx
@@ -1,5 +1,4 @@
 import Hero from "../sections/Hero.jsx";
-import ShowcaseSection from "../sections/ShowcaseSection.jsx";
 import NavBar from "../components/NavBar.jsx";
 import LogoSection from "../sections/LogoSection.jsx";
 import ExperienceSection from "../sections/ExperienceSection.jsx";
@@ -12,7 +11,6 @@ const PortfolioHome = () => {
             <Hero />
             <ExperienceSection />
             <TechStacks />
-            <ShowcaseSection />
             <LogoSection />
         </>
     )

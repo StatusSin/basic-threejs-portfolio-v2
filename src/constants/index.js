ca7f938ed55ec1487b7943c4daee4d0075a8ca82
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-07 21:56:54 -0500

    Cleaning

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,37 +26,37 @@ const counterItems = [
 
 const logoIconsList = [
     {
-        imgPath: "/images/logos/company-logo-1.png",
+        imgPath: "/images/LogosImages/company-logo-1.png",
     },
     {
-        imgPath: "/images/logos/company-logo-2.png",
+        imgPath: "/images/LogosImages/company-logo-2.png",
     },
     {
-        imgPath: "/images/logos/company-logo-3.png",
+        imgPath: "/images/LogosImages/company-logo-3.png",
     },
     {
-        imgPath: "/images/logos/company-logo-4.png",
+        imgPath: "/images/LogosImages/company-logo-4.png",
     },
     {
-        imgPath: "/images/logos/company-logo-5.png",
+        imgPath: "/images/LogosImages/company-logo-5.png",
     },
     {
-        imgPath: "/images/logos/company-logo-6.png",
+        imgPath: "/images/LogosImages/company-logo-6.png",
     },
     {
-        imgPath: "/images/logos/company-logo-7.png",
+        imgPath: "/images/LogosImages/company-logo-7.png",
     },
     {
-        imgPath: "/images/logos/company-logo-8.png",
+        imgPath: "/images/LogosImages/company-logo-8.png",
     },
     {
-        imgPath: "/images/logos/company-logo-9.png",
+        imgPath: "/images/LogosImages/company-logo-9.png",
     },
     {
-        imgPath: "/images/logos/company-logo-10.png",
+        imgPath: "/images/LogosImages/company-logo-10.png",
     },
     {
-        imgPath: "/images/logos/company-logo-11.png",
+        imgPath: "/images/LogosImages/company-logo-11.png",
     },
 ];
 

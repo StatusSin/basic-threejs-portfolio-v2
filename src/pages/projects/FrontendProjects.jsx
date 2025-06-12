c7239bd0f1c3cdc381d4600a583fb7f8a4a7bcee
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-12 12:13:00 -0500

    Update FrontendProjects.jsx

--- src/pages/projects/FrontendProjects.jsx
+++ src/pages/projects/FrontendProjects.jsx
@@ -11,6 +11,9 @@ const BackendProjects = () => {
             <p className="text-gray-400">
                 I have almost finished coding this part of the webpage to add my frontend projects.
             </p>
+            <p className="text-gray-400">
+                Here is the github link to the code for this webpage! https://github.com/aryamanram/Portfolio-Website
+            </p>
             <Link
                 to="/"
                 className="mt-6 inline-block rounded-full bg-white px-6 py-2 text-base font-semibold text-black transition hover:scale-105 focus-visible:outline-none focus-visible:ring focus-visible:ring-white/50"
